b5f7c24c9e3397c5a688a56396c0bf84522533af
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-27 17:33:33 -0600

    Commented out a lot of stand-in elements, to use as examples.
    Adjusted the grabUrl to use imperial measurements. And added a placeholder function to verify that the fetch is working properly.

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 // API use here, so at least one use of the fetch API script, possibly multiple.
 // May have to use multiple pages.
+var citiesList = document.getElementById("list-tab")
+var cityWeather = document.getElementById("nav-tabContent")
 
 var APIKey = "af02ea1d45601642cf73d29651dfd0dd"
-var lat = null
-var lon = null
-var city; // Stores the city as entered by user input.
-var grabUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// var lat = null
+// var lon = null --- Alternative means, probably less useful to the user, and out of scope of the MVP
+var city = "West+Jordan"
+var grabUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" +  APIKey;
 // "https://api.openweathermap.org/data/2.5/onecall?appid=af02ea1d45601642cf73d29651dfd0dd"
 
-function getWeather() {
-  // lat, lon coordinates required. As well as the appid. exclude, units, and lang are optional.
+console.log(grabUrl)
 
-  fetch(grabUrl)
-    .then(function (response) {
-      return response.json();
-    })
+fetch(
+  grabUrl
+)
+  .then(function(response) {
+    if (response.status !== 200) {
+      console.log("error")
+    }
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
 
-}
+
+// function getWeather() {
+
+//   fetch(grabUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function)
+    
+
+// }
 
 // Allows the 'list-tab' (placeholder) items to show their associated data when clicked.
 $('#list-tab a').on('click', function (e) {
