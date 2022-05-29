6ad696305ae5dfd41822ecb7d3c4d18c1ae7c325
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-29 17:55:11 -0600

    Added labels to the HTML to identify the temp, wind, etc.
    Added working event listener to the 'Search for a City' button.

--- script.js
+++ script.js
@@ -2,42 +2,64 @@
 // May have to use multiple pages.
 var citiesList = document.getElementById("list-tab")
 var cityWeather = document.getElementById("nav-tabContent")
+var citySubmit = document.getElementById("button-addon2")
+var userCityInput = document.getElementById("user-city")
 
-var APIKey = "af02ea1d45601642cf73d29651dfd0dd"
+var APIKey1 = "af02ea1d45601642cf73d29651dfd0dd"
+var APIKey2 = "6af1e54c068aac1b96a65def32f165a1"
 // var lat = null
 // var lon = null --- Alternative means, probably less useful to the user, and out of scope of the MVP
-var city = "West+Jordan"
-var grabCurrentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + APIKey;
-var grabForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid" + APIKey;
 
-console.log(grabCurrentUrl)
+var anchorAttributes = {
+  class: 'list-group-item list-group-item-action',
+  id: 'list',
+}
+
+function convertString() {
+  userInput = userCityInput.value.split(" ").join("+").toLowerCase();
+}
 
 
-// For now, this is acting on page load.
-fetch(
-  grabCurrentUrl
-)
-  .then(function(response) {
-    if (response.status !== 200) {
-      console.log("error")
+function setAttributes(element, anchorAttributes) {
+  Object.keys(anchorAttributes).forEach(attr => {
+    element.setAttribute(attr, attributes[attr]);
+  });
+}
+
+function getCurrentWeather(grabCurrentUrl) {
+  var grabCurrentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial" + "&appid=" + APIKey1
+
+  fetch(grabCurrentUrl)
+  .then(function (response) {
+    console.log(response.status)
+    if (response.status === 404) {
+      console.log("City not found!")
     }
     return response.json();
   })
-  .then(function (data) {
+  .then (function (data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      
+    }
+  });
+}
+
+function getFiveDay(grabForecastUrl) {
+  var grabForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&units=imperial" + "&appid=" + APIKey2
+
+  fetch(grabForecastUrl)
+  .then(function (response) {
+    console.log(response.status)
+    if (response.status !== 200) {
+      console.log("bad request")
+    }
+    return response.json();
+  })
+  .then (function (data) {
     console.log(data);
   })
-
-
-// function getWeather() {
-
-//   fetch(grabUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function)
-    
-
-// }
+}
 
 // Allows the 'list-tab' (placeholder) items to show their associated data when clicked.
 $('#list-tab a').on('click', function (e) {
@@ -45,5 +67,27 @@ $('#list-tab a').on('click', function (e) {
   $(this).tab('show')
 })
 
+// Click event for the submit button.
+$(citySubmit).on('click', function (e) {
+  if (userCityInput.value.length == 0) {
+    console.log("Nothing here")
+  }
+  convertString()
+  console.log(userInput)
+  e.preventDefault()
+  getCurrentWeather()
+  getFiveDay()
+  userCityInput.value = ""
+  userCityInput.placeholder = "If no information appears, city not found."
+})
+
 // Refer to: 
 // https://openweathermap.org/current#name
+
+
+// W3 example for removing spaces and filling them with + operator, useful for URL strings.
+// let text = "How are you doing today?";
+// const myArray = text.split(" ").join("+");
+// How+are+you+doing+today?
+// Also --join("+").toLowerCase(); -- and .trim() to remove spaces before and after. Clean user input
+// returns how+are+you+doing+today?
