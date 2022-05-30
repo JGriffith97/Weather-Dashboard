9261eef6da2c4cb17e5402b9706f8417c3c8e5bf
Jayden Griffith <griffithjayden97@outlook.com>
2022-05-29 23:51:42 -0600

    Got the UV-index working, along with the associated color-picking. Also got the icons working, and positioned properly next to the city name.
    Gonna need media queries next, or some bootstrap breakpoints to prevent overflow

--- script.js
+++ script.js
@@ -4,16 +4,11 @@ var citiesList = document.getElementById("list-tab")
 var cityWeather = document.getElementById("nav-tabContent")
 var citySubmit = document.getElementById("button-addon2")
 var userCityInput = document.getElementById("user-city")
-var cityName = document.getElementById("weatherHead")
+var cityName = document.getElementById("cityName")
 
 var APIKey = "6af1e54c068aac1b96a65def32f165a1"
-// var lat = null
-// var lon = null --- Alternative means, probably less useful to the user, and out of scope of the MVP
 
-var anchorAttributes = {
-  class: 'list-group-item list-group-item-action',
-  id: 'list',
-}
+var grabOneCall;
 
 function convertString() {
   userInput = userCityInput.value.trim().split(" ").join("+").toLowerCase();
@@ -28,7 +23,6 @@ function setAttributes(element, anchorAttributes) {
 
 function getCurrentWeather(grabCurrentUrl) {
   var grabCurrentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&units=imperial" + "&appid=" + APIKey
-
 // https://api.openweathermap.org/data/2.5/weather?q=west+jordan&units=imperial&appid=6af1e54c068aac1b96a65def32f165a1
 
   fetch(grabCurrentUrl)
@@ -40,18 +34,56 @@ function getCurrentWeather(grabCurrentUrl) {
   })
   .then (function (data) {
     console.log(data);
-      var temperature = document.getElementById("temp-span")
-      var windy = document.getElementById("wind-span")
-      var humid = document.getElementById("humidity-span")
-      var uvIndex = document.getElementById("uvindex-span")
-      // Note that naming the variable the same as the value given by the API causes issues
 
-      cityName.textContent = data.name + " " + data.weather[0].icon
-      temperature.textContent = data.main.temp
-      windy.textContent = data.wind.speed + " MPH"
-      humid.textContent = data.main.humidity + "%"
-      uvIndex.textContent = data
+    var temperature = document.getElementById("temp-span")
+    var windy = document.getElementById("wind-span")
+    var humid = document.getElementById("humidity-span")
+    var uvIndex = document.getElementById("uv-index-span")
+    // Note that naming the variable the same as the value given by the API causes issues
 
+    var iconCode = data.weather[0].icon
+    var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+
+    $('#cityName').after('<img id= "weather-icon" src="">')
+
+    cityName.textContent = data.name + " "
+    $("#weather-icon").attr("src", iconUrl)
+    temperature.textContent = data.main.temp
+    windy.textContent = data.wind.speed + " MPH"
+    humid.textContent = data.main.humidity + "%"
+
+    grabOneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&units=imperial" + "&appid=" + APIKey
+
+    fetch(grabOneCall)
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log("Well this is annoying")
+      }
+      return response.json();
+    })
+    .then (function (data) {
+      console.log(data)
+
+      uvIndex.textContent = data.current.uvi
+      if (uvIndex.textContent <= 2) {
+        uvIndex.style.backgroundColor = "lightgreen"
+        uvIndex.style.borderRadius = "8px"
+      } else if (uvIndex.textContent > 2 && uvIndex.textContent <= 5) {
+        uvIndex.style.backgroundColor = "lightsalmon"
+        uvIndex.style.borderRadius = "8px"
+      } else if (uvIndex.textContent > 5 && uvIndex.textContent <= 7) {
+        uvIndex.style.backgroundColor = "darkorange"
+        uvIndex.style.borderRadius = "8px"
+      } else if (uvIndex.textContent > 7 && uvIndex.textContent <= 10) {
+        uvIndex.style.color = "white"
+        uvIndex.style.backgroundColor = "darkred"
+        uvIndex.style.borderRadius = "8px"
+      } else if (uvIndex.textContent >= 11) {
+        uvIndex.style.color = "white"
+        uvIndex.style.backgroundColor = "rebeccapurple"
+        uvIndex.style.borderRadius = "8px"
+      }
+    })
   });
 }
 
@@ -72,6 +104,22 @@ function getFiveDay(grabForecastUrl) {
     console.log(data);
   })
 }
+
+
+// function getOneCall(grabOneCall) {
+
+
+//   fetch(grabOneCall)
+//   .then(function (response) {
+//     if (response.status === 404) {
+//       console.log("City not found!")
+//     }
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data)
+//   })
+// }
 
 // Allows the 'list-tab' (placeholder) items to show their associated data when clicked.
 $('#list-tab a').on('click', function (e) {
